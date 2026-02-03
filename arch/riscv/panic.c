a3faffeb66d40720e2c1c67b506ee98e17c25281
imbluedabr <baldergroen@gmail.com>
2026-02-03 15:10:19 +0100

    improved kernel panic output

--- arch/riscv/panic.c
+++ arch/riscv/panic.c
@@ -6,18 +6,20 @@
 #include <stdint.h>
 
 void panic(void) {
-    uint32_t sp;
+    uint32_t pc;
     __asm__ volatile (
-        "mv %0, sp"
-        : "=r" (sp)
+        "auipc %0, 0"
+        : "=r" (pc)
     );
     
-    freestanding_tty_puts("KERNEL PANIC:\nSP $\n");
-    freestanding_tty_puts(u32_to_hex(sp));
-    freestanding_tty_puts("\nPROC:\nPID: $");
+    freestanding_tty_puts("KERNEL PANIC:\nPC $");
+    freestanding_tty_puts(u32_to_hex(pc));
+    freestanding_tty_puts("\nPID: $");
     freestanding_tty_puts(u32_to_hex(current_process->pid));
     freestanding_tty_puts("\nSYSCALL: $");
     freestanding_tty_puts(u32_to_hex(current_process->syscall_operation));
+    freestanding_tty_puts("\nRA: $");
+    freestanding_tty_puts(u32_to_hex(current_process->return_address));
     
     while (1);
 }
